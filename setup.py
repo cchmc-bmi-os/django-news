6b02f42d5b98e51266b82afcfee4ce47b3d5e71f
Marcus Renno <renno.marcus@gmail.com>
2017-10-31 14:17:02 -0400

    Bumped version. Updated repo info

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ except ImportError:
     from setuptools.command.test import test
 
 setup(name = 'django-news',
-      description = "Fork of monokrome's lightweight news app",
-      author = 'Byron Ruth, Kevin Murphy, Brandon R. Stoner (orig)',
-      author_email = 'b@devel.io',
-      version = '0.7.1',
+      description = "Fork of CHOP's fork of monokrome's lightweight news app",
+      author = 'Marcus Renno',
+      author_email = 'marcus.renno@ccmc.org',
+      version = '0.7.2',
 
       zip_safe = False,
       include_package_data = True,
       packages = find_packages(),
-      url = 'http://github.com/cbmi/django-news/',
+      url = 'https://github.com/cchmc-bmi-os/django-news',
 
       classifiers = [
           "Development Status :: 4 - Beta",
